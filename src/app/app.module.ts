48bcf164f74d9d603511a5cdfb1bf78c75518b1d
santhosh <santhosh.k@rapyder.com>
2020-02-18 11:01:55 +0530

    Selector Types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SelectorComponent } from './selector/selector.component';
+import { ClassSelectorComponent } from './class-selector/class-selector.component';
+import { AttributeSelectorComponent } from './attribute-selector/attribute-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectorComponent,
+    ClassSelectorComponent,
+    AttributeSelectorComponent
   ],
   imports: [
     BrowserModule
